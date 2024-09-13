88a80c6c141ce40ebc822d55180edbec3b923483
Эдуард <khidiyatov2000@bk.ru>
2024-09-13 11:24:01 +0500

    objects and classes

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,11 @@ public class Main {
         Author bookTwo = new Author("Александр", "Грибоедов");
         Book one = new Book("Дубровский", bookOne, 1841);
         Book two = new Book("Горе от ума", bookTwo, 1833);
-        System.out.println("Название книги " + one.getBookName() + ". Автор " + bookOne.getFirstName() + " " + bookOne + main(1841));
-        System.out.println("Название книги " + two.getBookName() + ". Автор " + bookTwo.getFirstName() + " " + bookTwo + main(1833));
-        System.out.println("Название книги " + two.getBookName() + ". Автор" + bookTwo.getFirstName() + " " + bookTwo + main(1833));
+        System.out.println("Название книги " + one.getBookName() + ". Автор " + bookOne.getFirstName() + " " + bookOne + setbookAge(1841));
+        System.out.println("Название книги " + two.getBookName() + ". Автор " + bookTwo.getFirstName() + " " + bookTwo + setbookAge(1833));
+        System.out.println("Название книги " + two.getBookName() + ". Автор" + bookTwo.getFirstName() + " " + bookTwo + setbookAge(1833));
     }
-}
+
+    private static String setbookAge(int i) {
+    }
+
