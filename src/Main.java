caadc028b3d90413f8654564ec7689c54ee006a7
Эдуард <khidiyatov2000@bk.ru>
2024-09-13 11:16:53 +0500

    objects and classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author bookOne = new Author("Александр", "Пушкин");
+        Author bookTwo = new Author("Александр", "Грибоедов");
+        Book one = new Book("Дубровский", bookOne, 1841);
+        Book two = new Book("Горе от ума", bookTwo, 1833);
+        System.out.println("Название книги " + one.getBookName() + ". Автор " + bookOne.getFirstName() + " " + bookOne + main(1841));
+        System.out.println("Название книги " + two.getBookName() + ". Автор " + bookTwo.getFirstName() + " " + bookTwo + main(1833));
+        System.out.println("Название книги " + two.getBookName() + ". Автор" + bookTwo.getFirstName() + " " + bookTwo + main(1833));
     }
 }
